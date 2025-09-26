26fd61238db437ae174a69c684afba7c85bbe58b
Ylenia <yleniamighela@gmail.com>
2025-09-26 10:39:26 +0200

    draft AboutMe

---
+++ src/pages/AboutMe/AboutMe.jsx
@@ -0,0 +1,20 @@
+
+import "../AboutMe/AboutMe.css";
+
+export default function AboutMe() {
+    return (
+        <div className="container-about">
+
+            <div className="container-description">
+                🎓 Ho intrapreso un nuovo percorso professionale nel mondo dello sviluppo web, acquisendo competenze nello sviluppo di applicazioni moderne e responsive.
+                <em>🎨 Unendo creatività </em>
+                <em>🔍 Curiosità </em>
+                <em>🚀 Problem solving per realizzare soluzioni funzionali, accessibili e dall’esperienza utente curata.</em>
+                📚 Credo fortemente nell’apprendimento continuo e nella condivisione delle conoscenze come strumenti per crescere e migliorare costantemente.
+
+            </div>
+
+
+        </div>
+    );
+}
