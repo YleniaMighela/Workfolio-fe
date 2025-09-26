0d8f79f8897cf247a3126ba23c0833e531a95063
Ylenia <yleniamighela@gmail.com>
2025-09-26 10:40:09 +0200

    fixed App

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 // importo react-router
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+
 // importo il layout
 import DefaultLayout from "./layout/DefaultLayout";
 
@@ -8,20 +9,23 @@ import DefaultLayout from "./layout/DefaultLayout";
 import ProjectDetail from "./components/ProjectDetail/ProjectDetail"
 import ProjectList from "./components/ProjectList/ProjectList";
 
-// importo le paginr
-import HomePage from "./pages/HomePage";
+// importo le pagine
+import HomePage from "./pages/HomePage/HomePage";
+import AboutMe from "./pages/AboutMe/AboutMe";
+
 
 function App() {
 
   return (
     <>
+
       <BrowserRouter>
         <Routes>
           <Route element={<DefaultLayout />} >
             <Route path="/" element={<HomePage />} />
             <Route path="/projects" element={<ProjectList />} />
             <Route path="/projects/:id" element={<ProjectDetail />} />
-            {/* <Route path="/aboutme" element={<AboutMe />} /> */}
+            <Route path="/aboutme" element={<AboutMe />} />
 
           </Route>
         </Routes>
